8f192b6bb4fcf8354d9f8ce4d4eea97a444ccd60
elliottjjackson <ejay01@gmail.com>
2022-01-06 16:53:03 +0800

    Added speedtest result parser for db use

--- internet_speed_monitor/main.py
+++ internet_speed_monitor/main.py
@@ -1,6 +1,6 @@
 import logging
 from pathlib import Path
-from typing import Any, Callable
+from typing import Any, Callable, List, MutableMapping
 
 import speedtest as st
 
@@ -21,7 +21,7 @@ log = logging.getLogger(__name__)
 def add_log_header(message: str) -> None:
     """Add log header to log."""
     with open(log_filename, "a") as f:
-        f.write(f"\n\n<<< {message} >>>\n")
+        f.write(f"\n<<< {message} >>>\n")
 
 
 def plog(logging_level: int, object_to_log: Any) -> None:
@@ -57,10 +57,12 @@ def speedtest_connection_log(func: Callable[..., Any]) -> Callable[..., Any]:
 
 class SpeedTest:
     def __init__(self) -> None:
+        # Retrieve server stats, download and upload on first call.
         self.speed_test = st.Speedtest()
         self._server_stats = self.update_best_server()
         self._download_speed = self.update_download_speed()
         self._upload_speed = self.update_upload_speed()
+        self._results = self.speed_test.results
 
     @property
     def server_stats(self) -> dict[str, Any]:
@@ -73,6 +75,11 @@ class SpeedTest:
     @property
     def upload_speed(self) -> dict[str, Any]:
         return self._upload_speed
+
+    @property
+    def results(self) -> dict[str, Any]:
+        self._results = self.parse_speedtest_results(self._results.__dict__)
+        return self._results
 
     @speedtest_connection_log
     def update_best_server(self) -> dict[str, Any]:
@@ -92,6 +99,36 @@ class SpeedTest:
         self._upload_speed = self.speed_test.upload()
         return self._upload_speed
 
+    def parse_speedtest_results(
+        self, input_dict: MutableMapping[Any, Any], parent_key: str = "", sep: str = "_"
+    ) -> dict[str, Any]:
+        """Parses result set by flattening nested dictionaries.
+        Expected result is a dictionary with primitive values only and
+        concatenated keys (underscore deliminiter by default)."""
+        output_dict: List[tuple[str, Any]] = []
+        for k, v in input_dict.items():
+            new_key = parent_key + sep + k if parent_key else k
+            if isinstance(v, MutableMapping):
+                output_dict.extend(
+                    self.parse_speedtest_results(v, new_key, sep=sep).items()
+                )
+            else:
+                output_dict.append((new_key, v))
+        self._result: dict[str, Any] = dict(output_dict)
+        return self._result
+
+
+# TODO database handler class
+#   TODO Initialise database in project directory
+#   TODO Add new test data method
+#   TODO Delete last line method
+#   TODO delete all lines method
+
+# TODO Visualiser class
+#   TODO Pull data method, construct table
+#   TODO Create key data graph
+#   TODO Layout Graphs
+#   TODO Tabulate data
 
 if __name__ == "__main__":
 
@@ -100,4 +137,7 @@ if __name__ == "__main__":
     server_stats = speed_test.server_stats
     download = speed_test.download_speed
     upload = speed_test.upload_speed
+    results = speed_test.results
     add_log_header(f"SCRIPT {script_filename} COMPLETE")
+
+print(results)
